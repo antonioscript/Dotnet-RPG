d551eb4f835ebfd9e0bb2f9b37933d8da6bb5bd3
Antônio Rocha <antoniojunior159@gmail.com>
2022-08-12 20:24:56 -0300

    ConnectionString

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 global using Dotnet_RPG.Models;
+using Dotnet_RPG.Data;
 using Dotnet_RPG.Services.CharacterService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
