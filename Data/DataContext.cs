c2d9c08e231097f93ac867a6832fdaffd79199d8
Antônio Rocha <antoniojunior159@gmail.com>
2022-08-12 16:46:58 -0300

    Criado o arquivo DataContext.cs

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_RPG.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Character> Characters { get; set; }
+    }
+}
